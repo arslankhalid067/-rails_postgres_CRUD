6f709fbba8788a6d2236754712a3693c43f0867c
Arslan Khalid <arslankhalid067@gmail.com>
2020-02-26 19:48:58 +0500

    Implemented Devise Gem for authentication and showing articles to the user that were created by specific user

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,5 +2,6 @@ class Article < ApplicationRecord
     has_many :comments, dependent: :destroy
     belongs_to :user
     validates :title , presence: true
+    validates :body , presence: true
     has_one_attached :image
 end
